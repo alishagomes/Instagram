16927b72335439b5aad85693384f60a758934ac2
Alisha Gomes <alishagomes@fb.com>
2022-06-13 16:54:29 -0700

    Initial commit

---
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -0,0 +1,23 @@
+package com.example.instagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("aPAkiflfdHntjj2eBdpochEuSHJEL0uMvkSwxNj7")
+                .clientKey("f33K8O00XiSvyqOdlDiDccK2kv5DsCYufyltUPba")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
